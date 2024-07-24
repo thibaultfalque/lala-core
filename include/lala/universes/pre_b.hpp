8ddfb4a266c5bd90eaf5449d170be3e55bd47654
Pierre Talbot <pierre.talbot@uni.lu>
2024-07-24 16:19:36 +0200

    Improve PreB def.

--- include/lala/universes/pre_b.hpp
+++ include/lala/universes/pre_b.hpp
@@ -10,17 +10,19 @@ namespace lala {
 struct PreBD;
 
 /** `PreB` is a domain abstracting the Boolean universe of discourse \f$ \mathbb{B}=\{true,false\} \f$.
-    We overload the symbols \f$ \mathit{true} \f$ and \f$ \mathit{false} \f$ to be used in both `PreB` and the concrete domain (\f$ \bot, \top \f$ can be used to specifically refer to PreB elements).
-    We have \f$ PreB \triangleq \langle \{\mathit{false}, \mathit{true}\}, \implies, \land, \lor, \mathit{false}, \mathit{true} \rangle \f$ with the usual logical connectors (in particular, \f$ \mathit{false} \implies \mathit{true} \f$, \f$ \bot = \mathit{false} \f$ and \f$ \top = \mathit{true} \f$).
+    We have \f$ PreB \triangleq \langle \{f,t\}, \implies, \lor, \land, f, t \rangle \f$ where the order, join and meet operations are given by the usual Boolean logical connectors.
+    Picturally, we have f as the bottom element and t as the top element.
+    As shown by the Galois connection below, the element `t` does not represent the truth value but rather the unknown value (either true or false).
+    Interpreting `t` as true to implement the lattice operations is only a matter of convenience since those are the same than the logical operations when viewing `t` as true.
+    It is also useful to obtain the dual lattice \f$ PreBD \triangleq \langle \{f,t\}, \Leftarrow, \land, \lor, t, f \rangle \f$ by reusing already implemented operations.
+    However, it becomes different when we consider functions (`fun<sig>`) over this domain, as those might be different in `PreB` and `PreBD`.
+    For the implication, we have \f$ x \implies y \triangleq t \f$ in PreB for any values of x and y, but we have \f$ x \implies y \triangleq (x = t /\ y = t) ? t : f \f$ in PreBD, hence both cases need to be handled separately and no duality can be used (the function \f$ \implies \f$ is computed w.r.t. the concrete domain, which is not the same than the order of this lattice).
 
     We have a Galois connection between the concrete domain of values \f$ \mathcal{P}(\mathbb{B}) \f$ and PreB:
-    * Concretization: \f$ \gamma(b) \triangleq b = \top ? \{\mathit{true}, \mathit{false}\} : \{\mathit{false}\} \f$.
-    * Abstraction: \f$ \alpha(S) \triangleq \mathit{true} \in S ? \top : \bot \f$.
+    * Concretization: \f$ \gamma(b) \triangleq b = t ? \{\mathit{true}, \mathit{false}\} : \{\mathit{false}\} \f$.
+    * Abstraction: \f$ \alpha(S) \triangleq \mathit{true} \in S ? t : f \f$.
 
-    Beware that, as suggested by the concretization function, \f$ \top \f$ represents both the true and false values in the concrete domain, and should be interpreted as "I don't know yet the value".
-
-    Further, note that this lattice is unable to represent exactly Dunn/Belnap logic which requires four states (which is necessary when working in the abstract): unknown, true, false and failed.
-    To obtain Dunn/Belnap logic with a knowledge ordering, you can use `Interval<Bound<PreB>>`.
+    Note that by taking this lattice and its dual (using `Interval<Bound<PreB>>`), you can obtain Dunn/Belnap logic with a knowledge ordering.
 */
 struct PreB {
   using this_type = PreB;
@@ -43,17 +45,19 @@ struct PreB {
   CUDA constexpr static value_type one() { return true; }
 
   /** @sequential
-   * Interpret a formula into the PreB lattice.
-   * \return `true` if an overapproximation of a Boolean constant `b` could be placed in `tell`. Otherwise it returns `false` with a diagnostic. */
+   * Interpret a formula \f$ x => \mathit{false} \f$ into the PreB lattice.
+   * \return `true` if `f` is the false constant. Otherwise it returns `false` with a diagnostic. */
   template<bool diagnose, class F, bool dualize=false>
   CUDA static bool interpret_tell(const F& f, value_type& tell, IDiagnostics& diagnostics) {
     if(f.is(F::B)) {
-      if(f.b() == dualize) {
-        if constexpr(dualize) {
-          INTERPRETATION_WARNING("Overapproximating the constant `false` by the top element (which concretization gives {true, false}) in the `PreBD` domain.");
+      if constexpr(dualize) {
+        if(!f.b()) {
+          INTERPRETATION_ERROR("The constant `false` would be overapproximated by the top element (which concretization gives {true, false}) in the `PreBD` domain.");
         }
-        else {
-          INTERPRETATION_WARNING("Overapproximating the constant `true` by the top element (which concretization gives {true, false}) in the `PreB` domain.");
+      }
+      else {
+        if(f.b()) {
+          INTERPRETATION_ERROR("The constant `true` would be overapproximated by the top element (which concretization gives {true, false}) in the `PreB` domain.");
         }
       }
       tell = f.b();
@@ -63,26 +67,12 @@ struct PreB {
   }
 
   /** @sequential
-   * We can only ask if an element of this lattice is `false`, because it cannot exactly represent `true`.
+   * We can only ask \f$ x => \mathit{false} \f$ if an element of this lattice is `false`, because it cannot exactly represent `true`.
    * This operation can be dualized.
   */
   template<bool diagnose, class F, bool dualize=false>
   CUDA static bool interpret_ask(const F& f, value_type& ask, IDiagnostics& diagnostics) {
-    if(f.is(F::B)) {
-      if(f.b() == dualize) { /** In the dual, we can only ask for `true` elements. */
-        tell = f.b();
-        return true;
-      }
-      else {
-        if constexpr(dualize) {
-          RETURN_INTERPRETATION_ERROR("This Boolean domain can only represent two values: `true` or 'false'.");
-        }
-        else {
-          RETURN_INTERPRETATION_ERROR("This Boolean domain can only represent two values: `false` or 'I don't know'.");
-        }
-      }
-    }
-    RETURN_INTERPRETATION_ERROR("Only constant of types `Bool` can be interpreted in Boolean domains.");
+    return interpret_tell<diagnose, F, dualize>(f, ask, diagnostics);
   }
 
   /** @sequential
@@ -106,17 +96,15 @@ struct PreB {
 
   /** @parallel
    * Given a Boolean value, create a logical constant representing that value.
-   * Note that the lattice order has no influence here.
   */
-  // template<class F>
-  // CUDA static F deinterpret(const value_type& v) {
-  //   return F::make_bool(v);
-  // }
+  template<class F>
+  CUDA static F formula_of_constant(const value_type& v) {
+    return F::make_bool(v);
+  }
 
   /** The logical predicate symbol corresponding to the order of this pre-universe.
-      We have \f$ a \leq_\mathit{BInc} b \Leftrightarrow a \leq b \f$.
-      \return The logical symbol `LEQ`. */
-  CUDA static constexpr Sig sig_order() { return LEQ; }
+      \return The logical symbol `IMPLY`. */
+  CUDA static constexpr Sig sig_order() { return IMPLY; }
 
   /** Converse nonimplication: we have a < b only when `a` is `false` and `b` is `true`. */
   CUDA static constexpr Sig sig_strict_order() { return LT; }
@@ -133,24 +121,25 @@ struct PreB {
   /** \return \f$ x \sqcap y \f$ defined as \f$ x \land y \f$. */
   CUDA static constexpr value_type meet(value_type x, value_type y) { return x && y; }
 
-  /** \return \f$ \mathit{true} \f$ if \f$ x \leq_\mathit{BInc} y \f$ where the order \f$ \mathit{false} \leq_\mathit{BInc} \mathit{true} \f$, otherwise returns \f$ \mathit{false} \f$.
-      Note that the order is the Boolean implication, \f$ x \leq y \Rightleftarrow x \Rightarrow y \f$. */
+  /** \return \f$ \mathit{true} \f$ if \f$ x \implies y \f$. */
   CUDA static constexpr bool order(value_type x, value_type y) { return !x || y; }
 
-/** \return \f$ \mathit{true} \f$ if \f$ x \leq_\mathit{BInc} y \f$ where the order \f$ \mathit{false} \leq_\mathit{BInc} \mathit{true} \f$, otherwise returns \f$ \mathit{false} \f$.
-      Note that the strict order is the Boolean converse nonimplication, \f$ x \leq y \Rightleftarrow x \not\Leftarrow y \f$. */
+/** \return \f$ \mathit{true} \f$ if \f$ x \nleftarrow y \f$ where the converse nonimplication is true only when \f$ \mathit{false} \nleftarrow \mathit{true} \f$, otherwise it is false. */
   CUDA static constexpr bool strict_order(value_type x, value_type y) { return !x && y; }
 
-  /**  From a lattice perspective, this function returns an element \f$ y \f$ such that \f$ y \f$ is a cover of \f$ x \f$.
+  /**  From a lattice perspective, this function returns an element \f$ y \f$ such that \f$ y \f$ is a cover of \f$ x \f$, or top if x is top.
 
     \return `true`. */
   CUDA static constexpr value_type next(value_type x) { return true; }
 
-  /** From a lattice perspective, this function returns an element \f$ y \f$ such that \f$ x \f$ is a cover of \f$ y \f$.
+  /** From a lattice perspective, this function returns an element \f$ y \f$ such that \f$ x \f$ is a cover of \f$ y \f$, or bot if x is bot.
 
    \return `false`. */
   CUDA static constexpr value_type prev(value_type x) { return false; }
 
+  /** We do not support function that trivially maps to the \f$ \top \f$ element.
+   * For instance, the logical negation `not` is not supported because it is defined by: \f$ \lnot x \triangleq x = \mathit{true} \f$, because the negation of false is overapproximated by \f$ \top \f$ representing \f$ {true,false} \f$ in the concrete domain, and the negation of true stays true ( \f$ \lnot {true,false} = {\lnot true, \lnot false} = {true,false} \f$).
+   */
   CUDA NI static constexpr bool is_supported_fun(Sig sig) {
     switch(sig) {
       case AND:
@@ -158,7 +147,22 @@ struct PreB {
       case IMPLY:
       case EQUIV:
       case XOR:
+      case EQ:
+      case NEQ:
+        return true;
       case NOT:
+      default:
+        return false;
+    }
+  }
+
+  CUDA NI static constexpr bool is_order_preserving(Sig sig) {
+    switch(sig) {
+      case AND:
+      case OR:
+      case IMPLY:
+      case EQUIV:
+      case XOR:
       case EQ:
       case NEQ:
         return true;
@@ -167,23 +171,17 @@ struct PreB {
     }
   }
 
-  template<Sig sig>
-  CUDA static constexpr value_type fun(value_type x) {
-    static_assert(sig == NOT, "Unsupported unary function.");
-    switch(sig) {
-      case NOT: return !x;
-      default: assert(0); return x;
-    }
-  }
-
-  template<Sig sig>
+  /** We support binary logical connectors.
+   *  * \f$ \lnot x \triangleq x = \mathit{true} \f$, simply because the negation of false is overapproximated by \f$ \top \f$ representing \f$ {true,false} \f$ in the concrete domain, and the negation of true stays true ( \f$ \lnot {true,false} = {\lnot true, \lnot false} = {true,false} \f$).
+   */
+  template<Sig sig, bool dualize=false>
   CUDA NI static constexpr value_type fun(value_type x, value_type y) {
     static_assert(sig == AND || sig == OR || sig == IMPLY || sig == EQUIV || sig == XOR,
       "Unsupported binary function.");
     switch(sig) {
-      case AND: return x && y;
-      case OR: return x || y;
-      case IMPLY: return !x || y;
+      case AND: return dualize ? x || y : x && y;
+      case OR: return dualize ? x && y : x || y;
+      case IMPLY: return dualize ? !y || x : !x || y;
       case EQUIV:
       case EQ: return x == y;
       case XOR:
